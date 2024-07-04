5616e0ee2aca21816d1b53f5d2b3dc10d7e4110a
mike91827 <mike91827.2015@gmail.com>
2024-07-04 00:41:12 -0500

    fixed another error relaated at adding schema and images

--- pages/addImages.js
+++ pages/addImages.js
@@ -22,25 +22,6 @@ async function addAllImages(client) {
     })
 }
 
-async function addPokemonSchema(client) {
-    const schemaConfig = {
-        class: 'Pokemon',
-        vectorizer: 'img2vec-neural',
-        vectorIndexType: 'hnsw',
-        moduleConfig: {
-            'img2vec-neural': {
-                imageFields: ['image'],
-            },
-        },
-        properties: [
-            { name: 'image', dataType: ['blob'] },
-            { name: 'text', dataType: ['string'] },
-        ],
-    };
-
-    await client.schema.classCreator().withClass(schemaConfig).do();
-}
-
 async function main() {
 
     const client = weaviate.client({
@@ -50,7 +31,7 @@ async function main() {
 
 
     try {
-        await addPokemonSchema(client);
+        
         await addAllImages(client)
         console.log('Schema added successfully');
     } catch (error) {
